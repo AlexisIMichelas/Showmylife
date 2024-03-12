6458873bdda57df2f208f2df6fb14445d02dc2f2
AMichelas <120266869+AMichelas@users.noreply.github.com>
2024-03-12 21:46:14 +0100

    Add User

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\RegistrationFormType; // Assurez-vous que cette ligne est présente
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
+
 }
